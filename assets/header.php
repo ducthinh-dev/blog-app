bed38b74a147bcb8586a762ad6c53b0a267444aa
dothinh-dev <dothinh.dev@gmail.com>
2021-12-04 04:52:00 +0700

    update 5.1
    modify feed

--- assets/header.php
+++ assets/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-light bg-light">
   <div class="container-fluid">
-    <span class="navbar-brand mb-0 h1">Hello <?php echo $user_data['userID']?>!</span>
+    <span class="navbar-brand mb-0 h1">Hello <?php echo $user_data['username']?>!</span>
     <form class="container-fluid justify-content-start">
         <button class="btn btn-sm btn-outline-secondary" type="button"><a href="../logout.php">Logout</a></button>
     </form>
